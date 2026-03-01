2b385ba110f084b8c587659d4f879dc331365cac
София Краевская <kraevskay8797@gmail.com>
2026-03-01 20:52:04 +0700

    checkpoint 1

---
+++ src/database/migrations/2026_03_01_134517_create_resources_table.php
@@ -0,0 +1,26 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('type'); 
+            $table->integer('capacity');
+            $table->string('location'); 
+            $table->decimal('price_per_hour', 8, 2);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
